ce47710ab026ea695a9b484fd4181a0a0941faea
James Aydemir <jaydemir@jaydemir-mbalchemysystems.com>
2020-04-07 14:02:22 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes01-jaydemir01dev"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://mppl39c7mi.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_wAsJLKqBU",
+    APP_CLIENT_ID: "512juvad5vn84c6er6dneare4j",
+    IDENTITY_POOL_ID: "us-east-1:18bb1c7b-a5f6-49f9-b895-f46708f53be1"
+  }
+};
